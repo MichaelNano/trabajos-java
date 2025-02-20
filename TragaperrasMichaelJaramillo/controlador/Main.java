1690e91fa8619eef746f3cc7a45594aa90914b26
MichaelNano <michael777dios@gmail.com>
2025-02-20 20:42:37 +0100

    Tragaperras en java

---
+++ TragaperrasMichaelJaramillo/controlador/Main.java
@@ -0,0 +1,15 @@
+package controlador;
+
+/*
+  Michael Jaramillo Palacio
+  29/01/2025
+  Descripcion: Programa que funciona como una tragaperra y se pueden bloquear
+  numeros y desbloquearlos.
+  No te salta el mensaje hasta que los x casillas de numeros esten todas con el
+  mismo numero.
+*/
+public class Main {
+    public static void main(String[] args) {
+        new CTraga();
+    }
+}
